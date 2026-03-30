963ef3a7b50c29e36716d7c723bb7727a67a37af
Ansh Chikara <chikara.ansh15@gmail.com>
2026-03-30 14:01:28 +0530

    Fix insertion sort implementation and input prompts

--- Manual Programs 30/26_insertion_sort.c
+++ Manual Programs 30/26_insertion_sort.c
@@ -1,21 +1,27 @@
+
 #include<stdio.h>
 int main(){
-int n,i,j,key;
-printf("Enter size:\n");
-int a[n];
+int n,i,j,temp,key;
+printf("Enter the No. of Elements:\n");
 scanf("%d",&n);
+int a[n];
+
+printf("Enter the Elements: \n");
 for(i=0;i<n;i++) 
-  scanf("%d",&a[i]);
+scanf("%d",&a[i]);
+
 for(i=1;i<n;i++){
 key=a[i];
 j=i-1;
-while(j>=0 && a[j]>key){
-a[j+1]=a[j];
-j--;
+
+while(j>=0&&a[j]>key){
+    a[j+1]=a[j];
+    j--;
 }
 a[j+1]=key;
 }
-for(i=0;i<n;i++) 
-  printf("%d ",a[i]);
+printf("Sorted Array: ");
+for(i=0;i<n;i++)
+printf("%d ",a[i]);
 return 0;
 }
